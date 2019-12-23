2d743411072fc1d0ea2dd4ce0572a5c28fd07827
Eugene Palmoff <mahou@shoujo.pw>
2019-12-23 17:32:14 +0300

    Migration to trust-dns 0.18

--- examples/srv.rs
+++ examples/srv.rs
@@ -1,13 +1,16 @@
 use hyper::{client::HttpConnector, Body, Client, StatusCode, Uri};
 use hyper_srv::ServiceConnector;
-use trust_dns_resolver::AsyncResolver;
+use tokio::runtime::Runtime;
+use trust_dns_resolver::TokioAsyncResolver;
 
-#[tokio::main]
-pub async fn main() {
-    let (resolver, resolver_fut) = AsyncResolver::from_system_conf().unwrap();
-    tokio::spawn(resolver_fut);
-    let client = Client::builder().build::<_, Body>(ServiceConnector::new(HttpConnector::new(), Some(resolver)));
-    let response = client.get(Uri::from_static("http://_http._tcp.mxtoolbox.com")).await.unwrap();
-    // Cloudfront returns 403 but at least we have resolved SRV uri correctly.
-    assert_eq!(response.status(), StatusCode::FORBIDDEN);
+pub fn main() {
+    let mut rt = Runtime::new().unwrap();
+    let handle = rt.handle().clone();
+    rt.block_on(async move {
+        let resolver = TokioAsyncResolver::from_system_conf(handle).await.unwrap();
+        let client = Client::builder().build::<_, Body>(ServiceConnector::new(HttpConnector::new(), Some(resolver)));
+        let response = client.get(Uri::from_static("http://_http._tcp.mxtoolbox.com")).await.unwrap();
+        // Cloudfront returns 403 but at least we have resolved SRV uri correctly.
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 #![deny(missing_docs)]
 
 use futures::{
-    ready,
+    future::BoxFuture,
     task::{Context, Poll},
     Future,
 };
@@ -21,8 +21,7 @@ use std::{error::Error, fmt, pin::Pin};
 use tokio::io::{AsyncRead, AsyncWrite};
 use trust_dns_resolver::{
     error::{ResolveError, ResolveErrorKind},
-    lookup::SrvLookupFuture,
-    AsyncResolver, BackgroundLookup,
+    TokioAsyncResolver,
 };
 
 /// A wrapper around Hyper's [`Connect`]or with ability to preresolve SRV DNS records
@@ -31,42 +30,62 @@ use trust_dns_resolver::{
 /// [`Connect`]: ../hyper/client/connect/trait.Connect.html
 #[derive(Debug, Clone)]
 pub struct ServiceConnector<C> {
-    resolver: Option<AsyncResolver>,
+    resolver: Option<TokioAsyncResolver>,
     inner: C,
 }
 
 impl<C> Service<Uri> for ServiceConnector<C>
 where
-    C: Service<Uri> + Clone + Unpin,
+    C: Service<Uri> + Clone + Unpin + Send + 'static,
     C::Response: AsyncRead + AsyncWrite + Connection + Unpin + Send + 'static,
     C::Error: Into<Box<dyn Error + Send + Sync>>,
     C::Future: Unpin + Send,
 {
     type Response = C::Response;
     type Error = ServiceError;
-    type Future = ServiceConnecting<C>;
+    type Future = ServiceConnecting<C::Response>;
 
     fn poll_ready(&mut self, ctx: &mut Context) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(ctx).map_err(ServiceError::inner)
     }
 
     fn call(&mut self, uri: Uri) -> Self::Future {
-        let fut = match (&self.resolver, uri.host(), uri.port()) {
-            (Some(resolver), Some(host), None) => {
-                let fut = resolver.lookup_srv(host);
-                ServiceConnectingKind::Preresolve {
-                    inner: self.inner.clone(),
-                    uri: Some(uri),
-                    fut,
-                }
-            },
-            _ => {
-                ServiceConnectingKind::Inner {
-                    fut: self.inner.call(uri),
-                }
-            },
+        let resolver = self.resolver.clone();
+        let mut inner = self.inner.clone();
+        let f = async move {
+            let uri = match (&resolver, uri.host(), uri.port()) {
+                (Some(resolver), Some(host), None) => {
+                    let res = resolver.srv_lookup(host).await;
+                    let response = res.map(Some).or_else(|err| {
+                        match err.kind() {
+                            ResolveErrorKind::NoRecordsFound {
+                                ..
+                            } => Ok(None),
+                            _unexpected => Err(ServiceError(ServiceErrorKind::Resolve(err))),
+                        }
+                    })?;
+                    match response.as_ref().and_then(|response| response.iter().next()) {
+                        Some(srv) => {
+                            let authority = format!("{}:{}", srv.target(), srv.port());
+                            let builder = Uri::builder().authority(authority.as_str());
+                            let builder = match uri.scheme() {
+                                Some(scheme) => builder.scheme(scheme.clone()),
+                                None => builder,
+                            };
+                            let builder = match uri.path_and_query() {
+                                Some(path_and_query) => builder.path_and_query(path_and_query.clone()),
+                                None => builder,
+                            };
+                            builder.build().map_err(ServiceError::inner)?
+                        },
+                        None => uri,
+                    }
+                },
+                _ => uri,
+            };
+            inner.call(uri).await.map_err(ServiceError::inner)
         };
-        ServiceConnecting(fut)
+        ServiceConnecting(Box::pin(f))
     }
 }
 
@@ -78,7 +97,7 @@ impl<C> ServiceConnector<C> {
     /// in a client (as it must be named and can not even be made into a trait object).
     ///
     /// [`ServiceConnector`]: struct.ServiceConnector.html
-    pub fn new(inner: C, resolver: Option<AsyncResolver>) -> Self {
+    pub fn new(inner: C, resolver: Option<TokioAsyncResolver>) -> Self {
         ServiceConnector {
             resolver,
             inner,
@@ -131,90 +150,21 @@ impl ServiceError {
     }
 }
 
-#[allow(clippy::large_enum_variant)]
-enum ServiceConnectingKind<C>
-where
-    C: Service<Uri> + Unpin,
-{
-    Preresolve {
-        inner: C,
-        uri: Option<Uri>,
-        fut: BackgroundLookup<SrvLookupFuture>,
-    },
-    Inner {
-        fut: C::Future,
-    },
-}
-
-impl<C> fmt::Debug for ServiceConnectingKind<C>
-where
-    C: Service<Uri> + Unpin,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("ServiceConnectingKind").finish()
-    }
-}
-
 /// This future represents a connection in progress returned by [`ServiceConnector`].
 ///
 /// [`ServiceConnector`]: struct.ServiceConnector.html
-#[derive(Debug)]
-pub struct ServiceConnecting<C>(ServiceConnectingKind<C>)
-where
-    C: Service<Uri> + Unpin;
+pub struct ServiceConnecting<S>(BoxFuture<'static, Result<S, ServiceError>>);
 
-impl<C> Future for ServiceConnecting<C>
-where
-    C: Service<Uri> + Unpin,
-    C::Response: AsyncRead + AsyncWrite + Connection + Unpin + Send + 'static,
-    C::Error: Into<Box<dyn Error + Send + Sync>>,
-    C::Future: Unpin + Send,
-{
-    type Output = Result<C::Response, ServiceError>;
+impl<S> fmt::Debug for ServiceConnecting<S> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("ServiceConnecting").finish()
+    }
+}
+
+impl<S> Future for ServiceConnecting<S> {
+    type Output = Result<S, ServiceError>;
 
     fn poll(mut self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
-        match &mut self.0 {
-            ServiceConnectingKind::Preresolve {
-                inner,
-                uri,
-                fut,
-            } => {
-                let res = ready!(Pin::new(fut).poll(ctx));
-                let response = res.map(Some).or_else(|err| {
-                    match err.kind() {
-                        ResolveErrorKind::NoRecordsFound {
-                            ..
-                        } => Ok(None),
-                        _unexpected => Err(ServiceError(ServiceErrorKind::Resolve(err))),
-                    }
-                })?;
-                let uri = uri.take().expect("double ready on preresolve future");
-                let uri = match response.as_ref().and_then(|response| response.iter().next()) {
-                    Some(srv) => {
-                        let authority = format!("{}:{}", srv.target(), srv.port());
-                        let builder = Uri::builder().authority(authority.as_str());
-                        let builder = match uri.scheme() {
-                            Some(scheme) => builder.scheme(scheme.clone()),
-                            None => builder,
-                        };
-                        let builder = match uri.path_and_query() {
-                            Some(path_and_query) => builder.path_and_query(path_and_query.clone()),
-                            None => builder,
-                        };
-                        builder.build().map_err(ServiceError::inner)?
-                    },
-                    None => uri,
-                };
-                {
-                    *self = ServiceConnecting(ServiceConnectingKind::Inner {
-                        fut: inner.call(uri),
-                    });
-                }
-                self.poll(ctx)
-            },
-            ServiceConnectingKind::Inner {
-                fut,
-            } => Pin::new(fut).poll(ctx).map_err(ServiceError::inner),
-        }
+        Pin::new(&mut self.0).poll(ctx)
     }
 }
