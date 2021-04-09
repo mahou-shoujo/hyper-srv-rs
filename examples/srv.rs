af07cb3e40ce3b26dcfb71f9df349f31e23bb9ae
Eugene Palmoff <mahou@shoujo.pw>
2021-04-09 17:53:34 +0300

    Update some deps *) hyper 0.13 -> 0.14 *) tokio 0.2 -> 1 *) trust-dns-proto 0.19 -> 0.20 *) trust-dns-resolver 0.19 -> 0.20

--- examples/srv.rs
+++ examples/srv.rs
@@ -4,7 +4,7 @@ use trust_dns_resolver::AsyncResolver;
 
 #[tokio::main]
 pub async fn main() {
-    let resolver = AsyncResolver::tokio_from_system_conf().await.unwrap();
+    let resolver = AsyncResolver::tokio_from_system_conf().unwrap();
     let client = Client::builder().build::<_, Body>(ServiceConnector::new(HttpConnector::new(), Some(resolver)));
     let response = client.get(Uri::from_static("http://_http._tcp.mxtoolbox.com")).await.unwrap();
     // Cloudfront returns 403 but at least we have resolved SRV uri correctly.
